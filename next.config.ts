ed3564fb3bf9c822e3d23c4407b4e8660e777d03
rachelwong <hello.rachelwong@gmail.com>
2026-08-13 12:12:54 +1000

    fix: updated base dependency configuration

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
 import type { NextConfig } from "next";
+const path = require('path')
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+      },
+    ],
   },
 };
 
